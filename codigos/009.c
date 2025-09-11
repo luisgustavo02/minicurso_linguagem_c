01e222b1a513afb68d543e2766d856195fd946c4
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:55:45 -0300

    Create 009.c

---
+++ codigos/009.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+	// Algumas das principais funcoes da biblioteca ctype
+	printf("toupper('g') = %c\n", toupper('g'));
+	printf("toupper('G') = %c\n\n", toupper('G'));
+	printf("tolower('g') = %c\n", tolower('g'));
+	printf("tolower('G') = %c\n", tolower('G'));
+	
+	return 0;
+}
