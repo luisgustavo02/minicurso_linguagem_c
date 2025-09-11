ad706322311e8acb3ad330aee43a391ff6d68fc5
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:59:09 -0300

    Create 012.c

---
+++ codigos/012.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main(){
+	/*
+	Funcao Sleep() recebe um valor em ms e para o programa nesse
+	periodo
+	*/
+	
+	Sleep(2000);
+	printf("Boa tarde pessoal!\n");
+	Sleep(2000);
+	printf("Tudo bem com voces?\n");
+	Sleep(2000);
+	printf("Como voces estao?");
+	
+	return 0;
+}
