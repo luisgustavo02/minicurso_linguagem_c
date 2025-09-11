e3ff9e30348e999f907fe88140f0e90621e69661
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:22:00 -0300

    Create 006.c

---
+++ codigos/006.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	/*
+	A funcao ceil() faz o arredondamento para cima
+	
+	Enquanto a funcao floor() faz o arredondamento para baixo
+	*/
+	printf("ceil(4.7) = %f\tceil(4.4) = %f\n", ceil(4.7), ceil(4.4));
+	printf("floor(4.7) = %f\tfloor(4.4) = %f", floor(4.7), floor(4.4));
+	
+	return 0;
+}
