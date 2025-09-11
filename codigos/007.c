34f46894523d2ad8c5de5dca95acb17d863f339f
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:29:36 -0300

    Create 007.c

---
+++ codigos/007.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	// Algumas das principais funcoes matematicas da biblioteca math
+	printf("sqrt(4) = %f\tsqrt(10) = %f\n", sqrt(4), sqrt(10));
+	printf("pow(4, 2) = %f\tpow(10, 3) = %f\n", pow(4, 2), pow(10, 3));
+	printf("exp(1) = %f\texp(2) = %f\n", exp(1), exp(2));
+	printf("log(e) = %f\tlog(2) = %f\n", log(exp(1)), log(2));
+	printf("log10(10) = %f\tlog10(2) = %f\n", log10(10), log10(2));
+	
+	return 0;
+}
