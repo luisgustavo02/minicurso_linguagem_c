07cb7212ed6b6112a00b7459fe31a394af99c608
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:08:21 -0300

    Create 005.c

---
+++ codigos/005.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	/*
+	Valores absolutos com a funcao abs() da biblioteca math
+	Funciona apenas para valores inteiros
+	*/
+	printf("abs(4) = %d\n", abs(4));
+	printf("abs(-2) = %d\n", abs(-2));
+  
+	return 0;
+}
