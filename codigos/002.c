30dc89f0bcfcc167734429f5052c8b23cb9e264e
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 08:20:33 -0300

    Create 002.c

---
+++ codigos/002.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+	printf("Boa tarde pessoal!\n");
+	/*
+	O "\n", quando utilizado como texto na funcao printf(),
+	permite que haja uma quebra de linha e o proximo texto
+	seja mostrado na linha abaixo
+	*/
+	printf("Tudo bom\tcom vocês?");
+	/*
+	Enquanto o "\t" funciona de forma parecida, mas da um
+	espaco de tabulacao
+	*/
+	
+	return 0;
+}
