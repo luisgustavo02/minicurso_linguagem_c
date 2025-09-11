4a1342b401ad8cbf501adbb80127bfb92efbe42a
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:49:15 -0300

    Create 008.c

---
+++ codigos/008.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	// Algumas das principais funcoes matematicas da biblioteca math
+	printf("pi = %f\n", M_PI);
+	printf("sin(pi/4) = %f\tsin(pi) = %f\n", sin(M_PI/4), sin(M_PI));
+	printf("cos(pi/4) = %f\tcos(pi) = %f\n", cos(M_PI/4), cos(M_PI));
+	printf("tan(pi/4) = %f\ttan(pi) = %f\n", tan(M_PI/4), tan(M_PI));
+	
+	return 0;
+}
