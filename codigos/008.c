6193f52c749781f65e888d7656784e04daa33bac
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-11 09:54:40 -0300

    Update 008.c

--- codigos/008.c
+++ codigos/008.c
@@ -3,10 +3,12 @@
 
 int main(){
 	// Algumas das principais funcoes matematicas da biblioteca math
-	printf("pi = %f\n", M_PI);
-	printf("sin(pi/4) = %f\tsin(pi) = %f\n", sin(M_PI/4), sin(M_PI));
-	printf("cos(pi/4) = %f\tcos(pi) = %f\n", cos(M_PI/4), cos(M_PI));
-	printf("tan(pi/4) = %f\ttan(pi) = %f\n", tan(M_PI/4), tan(M_PI));
+	printf("sqrt(4) = %f\tsqrt(10) = %f\n", sqrt(4), sqrt(10));
+	printf("pow(4, 2) = %f\tpow(10, 3) = %f\n", pow(4, 2), pow(10, 3));
+	printf("e = %f\n", M_E);
+	printf("exp(1) = %f\texp(2) = %f\n", exp(1), exp(2));
+	printf("log(e) = %f\tlog(2) = %f\n", log(exp(1)), log(2));
+	printf("log10(10) = %f\tlog10(2) = %f\n", log10(10), log10(2));
 	
 	return 0;
 }
