62544e949bdcf2bf85dd97831374fd46472d2140
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 19:11:56 -0300

    Create 018 - Estrutura if e else.c

---
+++ codigos/018 - Estrutura if e else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura if e else
+	int num1 = 1;
+	int num2 = 3;
+	
+	if (num1 == num2){
+		printf("A variavel num1 eh igual a variavel num2!");
+	}else{
+		printf("A variavel num1 eh diferente da variavel num2!");
+	}
+	
+	return 0;
+}
