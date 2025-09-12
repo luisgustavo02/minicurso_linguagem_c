a713a42ffda9c5ee106fc3a95c2e6decaf3903bb
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 18:56:48 -0300

    Create 014 - Operações com inteiros.c

---
+++ codigos/014 - Operações com inteiros.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	// Operacoes com inteiros resultam em inteiros
+	printf("Adicao: %d\n", 3+2);
+	printf("Subtracao: %d\n", 3-2);
+	printf("Multiplicacao: %d\n", 3*2);
+	printf("Divisao: %d\n", 3/2);
+	printf("Resto da divisao: %d\n", 3%2);
+	
+	return 0;
+}
