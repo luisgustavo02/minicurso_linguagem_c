117d263379b56244beba73e69af94573902f7dd2
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 19:05:37 -0300

    Create 017 - Operadores comparativos.c

---
+++ codigos/017 - Operadores comparativos.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	// Comparando valores
+	int num1 = 1;
+	int num2 = 3;
+	printf("num1 == num2 => %d\n", num1 == num2);
+	printf("num1 != num2 => %d\n", num1 != num2);
+	printf("num1 < num2 => %d\n", num1 < num2);
+	printf("num1 <= num2 => %d\n", num1 <= num2);
+	printf("num1 > num2 => %d\n", num1 > num2);
+	printf("num1 >= num2 => %d\n", num1 >= num2);
+	
+	return 0;
+}
