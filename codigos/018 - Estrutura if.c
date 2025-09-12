10c401b1e75daf5d653007b5d87c87760235c9db
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 19:15:39 -0300

    Create 018 - Estrutura if.c

---
+++ codigos/018 - Estrutura if.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura if
+	int num1 = 1;
+	int num2 = 3;
+	
+	if (num1 == num2){
+		printf("A variavel num1 eh igual a variavel num2!");
+	}
+	
+	return 0;
+}
