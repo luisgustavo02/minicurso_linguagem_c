2cc164ebfb18723862ced6af4c03c6c74e1f26a0
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 18:57:16 -0300

    Create 015 - Operações com pontos flutuantes.c

---
+++ codigos/015 - Operações com pontos flutuantes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	// Operacoes com pontos flutuantes resultam em pontos flutuantes
+	printf("Adicao: %f\n", 3.0+2.0);
+	printf("Subtracao: %f\n", 3.0-2.0);
+	printf("Multiplicacao: %f\n", 3.0*2.0);
+	printf("Divisao: %f\n", 3.0/2.0);
+	
+	return 0;
+}
