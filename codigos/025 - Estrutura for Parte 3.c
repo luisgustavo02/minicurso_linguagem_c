3f957546b37553d8fb787bca4a565ccea29f3def
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-13 09:44:25 -0300

    Update 025 - Estrutura for Parte 3.c

--- codigos/025 - Estrutura for Parte 3.c
+++ codigos/025 - Estrutura for Parte 3.c
@@ -4,7 +4,7 @@ int main(){
 	// Estrutura for
 	
 	int i, j;
-	// for (inicio; condicao de parada; passo)
+	// for (inicio; condicao de continuidade; passo)
 	for (i=4, j=1; i+j<15; i+=2, j--){
 		printf("%d %d\n", i, j);
 	}
