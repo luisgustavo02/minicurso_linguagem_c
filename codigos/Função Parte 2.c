c24e7e2968411e220d37ba89db3102875c68a116
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-13 16:43:11 -0300

    Create Função Parte 2.c

---
+++ codigos/Função Parte 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// Estrutura de funcao inteira
+float absVal(float n){
+	if (n > 0){
+		return n;
+	}else{
+		return -n;
+	}
+}
+
+int main(){
+	float n1 = -3.4, n2 = -99, n3 = 12, n4 = 0;
+	
+	printf("Funcao abs(-3.4) = %f\n", absVal(n1));
+	printf("Funcao abs(-99) = %f\n", absVal(n2));
+	printf("Funcao abs(12) = %f\n", absVal(n3));
+	printf("Funcao abs(0) = %f\n", absVal(n4));
+	
+	return 0;
+}
