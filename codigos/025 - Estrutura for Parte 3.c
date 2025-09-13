d0ac99bd809de0c847fb22d3be679336601de6fc
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 22:54:02 -0300

    Create 025 - Estrutura for Parte 3.c

---
+++ codigos/025 - Estrutura for Parte 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura do for
+	
+	int i, j;
+	// for (inicio; condicao de parada; passo)
+	for (i=4, j=1; i+j<15; i+=2, j--){
+		printf("%d %d\n", i, j);
+	}
+	
+	return 0;
+}
