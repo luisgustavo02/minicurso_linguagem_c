3666a6711ae49538fad38e00db4a57d5974bd6fb
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-13 09:56:32 -0300

    Create 029 - Estrutura do while Parte 1.c

---
+++ codigos/029 - Estrutura do while Parte 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura do while
+	
+	int i = 0;
+	// do {escopo} while (condicao de continuidade)
+	do{
+		printf("%d ", i);
+		i++;
+	}while (i<10);
+	
+	return 0;
+}
