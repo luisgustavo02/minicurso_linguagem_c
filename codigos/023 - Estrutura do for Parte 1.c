11abdb44d61d9e9609642636a7ba8697208b05ba
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-12 22:46:05 -0300

    Create 023 - Estrutura do for Parte 1.c

---
+++ codigos/023 - Estrutura do for Parte 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura do for
+	
+	int i = 0;
+	// for (inicio; condicao de parada; passo)
+	for (i=0; i<10; i++){
+		printf("%d ", i);
+	}
+	
+	return 0;
+}
