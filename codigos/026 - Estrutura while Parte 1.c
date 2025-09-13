142525a31b3e050200f4a54f131d05528326e9c6
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-13 09:47:17 -0300

    Create 026 - Estrutura while Parte 1.c

---
+++ codigos/026 - Estrutura while Parte 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	// Estrutura while
+	
+	int i = 0;
+	// while (condicao de continuidade)
+	while (i<10){
+		printf("%d ", i);
+		i++;			// i += 1 ou i = i + 1
+	}
+	
+	return 0;
+}
