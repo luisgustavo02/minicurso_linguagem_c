aca41d42b1cde2664ca50cc2d71a5de178f6def6
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2025-09-13 22:14:29 -0300

    Create 038 - Funções Parte 4.c

---
+++ codigos/038 - Funções Parte 4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+// Estrutura de funcao vazia
+void prints(int n){
+	int i;
+	for (i=0; i<n; i++){
+		printf("Boa tarde!\n");
+	}
+}
+
+int main(){
+	prints(7);
+	
+	return 0;
+}
