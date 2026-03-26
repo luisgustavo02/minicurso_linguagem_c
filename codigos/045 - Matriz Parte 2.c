8541e0ab95aa0f8db4d809cb92e914c9fb2ef93c
Luís Gustavo <54186119+luisgustavo02@users.noreply.github.com>
2026-03-26 08:58:39 -0300

    Create 045 - Matriz Parte 2.c

---
+++ codigos/045 - Matriz Parte 2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+	// Estruturas de uma matriz
+	int M = 5, N = 5;
+	int matriz[M][N];
+	
+	int i, j;
+	for (i=0; i<M; i++){
+		for (j=0; j<N; j++){
+			printf("Digite o valor da matriz na posicao [%d][%d]: ", i, j);
+			scanf(" %d", &matriz[i][j]);
+		}
+	}
+	
+	// Imprimindo a matriz transposta
+	for (i=0; i<M; i++){
+		for (j=0; j<N; j++){
+			printf("%d ", matriz[j][i]);
+		}
+		printf("\n");
+	}
+  
+	return 0;
+}
