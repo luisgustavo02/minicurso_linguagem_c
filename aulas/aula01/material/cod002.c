c970d58e8d30d04777684d455d627594a817e213
luisgustavo02 <lg.amaral.02@gmail.com>
2026-08-11 20:16:24 -0300

    Adicionando arquivo cod002.c

--- aulas/aula01/material/cod002.c
+++ aulas/aula01/material/cod002.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 int main(){
-    int idade;
-    
-    printf("Digite sua idade: ");
-    scanf(" %d", &idade);
 
-    printf("Sua idade eh: %d", idade);
+    printf("int: %ld\n", sizeof(int));
+    printf("long int: %ld\n", sizeof(long int));
+    printf("short int: %ld\n", sizeof(short int));
+    printf("float: %ld\n", sizeof(float));
+    printf("double: %ld\n", sizeof(double));
+    printf("long double: %ld\n", sizeof(long double));
+    printf("char: %ld\n", sizeof(char));
 
     return 0;
 }
