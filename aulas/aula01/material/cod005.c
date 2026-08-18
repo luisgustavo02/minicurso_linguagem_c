5acb2a1b6f98c6a4ac2cb7d949bd28ba7813730d
luisgustavo02 <lg.amaral.02@gmail.com>
2026-08-17 23:05:48 -0300

    Atualizando arquivo cod005.c

---
+++ aulas/aula01/material/cod005.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int num1 = 3, num2 = 4;
+    float num3 = 6.7, num4 = 9.3;
+    printf("%d + %d = %d\n", num1, num2, num1 + num2);
+    printf("%d + %f = %f\n", num1, num3, num1 + num3);
+    printf("%f + %f = %f", num3, num4, num3 + num4);
+
+    return 0;
+}
