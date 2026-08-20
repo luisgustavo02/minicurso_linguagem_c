89fad9b8a6742ac06ff4ea6b4638d4c7af992bbe
luisgustavo02 <lg.amaral.02@gmail.com>
2026-08-19 21:54:04 -0300

    Adicionando arquivo cod011.c

---
+++ aulas/aula01/material/cod011.c
@@ -0,0 +1,20 @@
+#include<math.h>
+#include<stdio.h>
+
+int main(){
+    printf("e: %.15f\n", M_E);
+    printf("log2(e): %.15f\n", M_LOG2E);
+    printf("log10(2): %.15f\n", M_LOG10E);
+    printf("ln(2): %.15f\n", M_LN2);
+    printf("ln(10): %.15f\n", M_LN10);
+    printf("pi: %.15f\n", M_PI);
+    printf("pi/2: %.15f\n", M_PI_2);
+    printf("pi/4: %.15f\n", M_PI_4);
+    printf("1/pi: %.15f\n", M_1_PI);
+    printf("2/pi: %.15f\n", M_2_PI);
+    printf("2/sqrt(pi): %.15f\n", M_2_SQRTPI);
+    printf("sqrt(2): %.15f\n", M_SQRT2);
+    printf("1/sqrt(2): %.15f\n", M_SQRT1_2);
+
+    return 0;
+}
